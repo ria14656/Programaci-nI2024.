8b7d3d61509b7478ecb86eba0d192030c81550ab
Ariatna Berganza <aberganzav@miumg.edu.gt>
2024-03-15 20:54:09 -0600

    nueva carpeta

---
+++ seman6/parte2problem4/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Desafio_1
+{
+    class Rectangulo
+    {
+        public double Alto { get; }
+        public double Largo { get; }
+
+        public double SuperficieFrontal
+        {
+            get { return Alto * Largo; }
+        }
+
+        public Rectangulo(double alto, double largo)
+        {
+            Alto = alto;
+            Largo = largo;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo = new Rectangulo(5, 10);
+
+            Console.WriteLine("Superficie frontal del rectángulo: {0}", rectangulo.SuperficieFrontal);
+        }
+    }
+}
---
+++ seman6/problem5/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CalculoVolumenCajas
+{
+    class Caja
+    {
+        public double Alto { get; set; }
+        public double Ancho { get; set; }
+        public double Profundidad { get; set; }
+
+        public double CalcularVolumen()
+        {
+            return Alto * Ancho * Profundidad;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuántas cajas deseas ingresar? ");
+            int cantidadCajas = int.Parse(Console.ReadLine());
+
+            double volumenTotal = 0;
+
+            for (int i = 1; i <= cantidadCajas; i++)
+            {
+                Console.WriteLine($"Caja {i}:");
+                Console.Write("Alto: ");
+                double alto = double.Parse(Console.ReadLine());
+                Console.Write("Ancho: ");
+                double ancho = double.Parse(Console.ReadLine());
+                Console.Write("Profundidad: ");
+                double profundidad = double.Parse(Console.ReadLine());
+
+                Caja caja = new Caja { Alto = alto, Ancho = ancho, Profundidad = profundidad };
+                volumenTotal += caja.CalcularVolumen();
+            }
+
+            double volumenPromedio = volumenTotal / cantidadCajas;
+
+            Console.WriteLine($" total de las cajas: {volumenTotal:F2}");
+            Console.WriteLine($"Volumen promedio de las cajas: {volumenPromedio:F2}");
+        }
+    }
+}
