e87cdab63922862b339e2ce6bf348d358d590114
Ariatna Berganza <aberganzav@miumg.edu.gt>
2024-04-12 22:12:50 -0600

    intento 1000

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string textoEjemplo = "Este es mi correo electrónico: ariatnav63@gmail.com. Por favor, envíeme un correo con sus cnsultas.";
+
+        // Expresión regular para encontrar direcciones de correos electrónicos
+        string patronCorreo = @"\b[\w\.-]+@[\w\.-]+\.\w+\b";
+
+        // Crear una instancia de la clase Regex con el patrón
+        Regex regex = new Regex(patronCorreo);
+
+        // Buscar todas las coincidencias en el texto
+        MatchCollection direccionesEncontradas = regex.Matches(textoEjemplo);
+
+        // Imprimir las direcciones encontradas
+        foreach (Match direccion in direccionesEncontradas)
+        {
+            Console.WriteLine(direccion.Value);
+        }
+    }
+}
